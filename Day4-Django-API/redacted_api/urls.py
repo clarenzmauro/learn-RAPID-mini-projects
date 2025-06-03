8f175c66a6a9a0ccfaa8c5c11de4ba6dbe5b177f
clarenzmauro <m27oflegend@gmail.com>
2025-06-03 10:12:45 +0800

    django API

---
+++ Day4-Django-API/redacted_api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'assessment_api' # Optional: for namespacing URLs
+
+urlpatterns = [
+    path('questions/', views.question_list_create_api, name='question-list-create'),
+    path('submit_answer/', views.submit_answer_api, name='submit-answer'),
+]
